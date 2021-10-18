480370654e12ebfa8a32d7e78ef11880fdb933b3
Tashnuva <tasnuvactg@yahoo.com>
2021-10-17 21:49:09 -0400

    Fibonacci

--- src/math/Fibonacci.java
+++ src/math/Fibonacci.java
@@ -7,7 +7,13 @@ public class Fibonacci {
          /*
           Write 40 Fibonacci numbers with java.
          */
-
+int prev = 0;
+int next = 1;
+for(int i = 0; i < 40; i++) {
+    System.out.println(prev);
+    prev = next - prev;
+    next = next + prev;
+}
 
     }
 }
