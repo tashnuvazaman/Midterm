0e5bf7195d9c9771bfe674b1e6a9d7c4465c4d80
Tashnuva <zamantashnuva@gmail.com>
2021-10-21 18:11:09 -0400

    Made Pyramid

--- src/math/MakePyramid.java
+++ src/math/MakePyramid.java
@@ -1,5 +1,7 @@
 package math;
 
+import java.util.Scanner;
+
 public class MakePyramid {
     public static void main(String[] args) {
 
@@ -14,6 +16,21 @@ public class MakePyramid {
 
 
         //implementation here...
+        int i, space, rows, k = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Number Of Rows: ");//heights of pyramid
+        rows = scan.nextInt();
+        for (i = 1; i <= rows; i++) {
 
+            for (space = 1; space <= (rows - i); space++) {
+                System.out.print(" ");
+            }
+            while (k != (2 * i - 1)) {
+                System.out.print("* ");
+                k++;
+            }
+            k = 0;
+            System.out.println();
+        }
     }
-  }
+}
