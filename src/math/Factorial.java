1988e54c84bc64a5a176696bd20a6927510db9c8
Tashnuva <zamantashnuva@gmail.com>
2021-10-21 22:55:32 -0400

    Factorial

--- src/math/Factorial.java
+++ src/math/Factorial.java
@@ -1,8 +1,8 @@
 package math;
 
-/**
- * Created by mrahman on 04/02/18.
- */
+
+import java.util.Scanner;
+
 public class Factorial {
 
     public static void main(String[] args) {
@@ -16,6 +16,31 @@ public class Factorial {
         //implementation here...
 
 
-    }
+        final String[] strings = args;
 
-}
+
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Enter the number: ");
+
+                int num = scanner.nextInt();
+
+                int factorial = fact(num);
+
+                System.out.println("Factorial of entered number is: "+factorial);
+            }
+            static int fact(int n)
+            {
+                int output;
+                if(n==1){
+                    return 1;
+                }
+
+                output = fact(n-1)* n;
+                return output;
+            }
+        }
+
+
+
+
