2920124e26763e55880d1fa14ccc349fe71e34d0
Tashnuva <zamantashnuva@gmail.com>
2021-10-21 23:20:53 -0400

    Duplicate Words Solved

--- src/problems/DuplicateWord.java
+++ src/problems/DuplicateWord.java
@@ -18,7 +18,28 @@ public class DuplicateWord {
 
         //implementation here...
 
+                int count;
+
+                String words[] = string.split(" ");
+
+                System.out.println("Duplicate words are : ");
+                for(int i = 0; i < words.length; i++) {
+                    count = 1;
+                    for(int j = i+1; j < words.length; j++) {
+                        if(words[i].equals(words[j])) {
+                            count++;
+
+                            words[j] = "0";
+                        }
+                    }
+
+
+                    if(count > 1 && words[i] != "0")
+                        System.out.println(words[i]);
+                }
+            }
+
 
     }
 
-}
+
